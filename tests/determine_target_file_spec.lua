b491327c79a2345e99644856b7c7682ee6b53715
Stefan Bradl <bradlstefan@googlemail.com>
2026-04-15 14:55:01 +0200

    corrected tests

--- tests/determine_target_file_spec.lua
+++ tests/determine_target_file_spec.lua
@@ -1,7 +1,10 @@
 local determine_target_file = require("matching-file")._determine_target_file
 
 local function assert_matching_file(expected_target, file)
-	assert.are.same("../testdata/" .. expected_target, determine_target_file("../testdata/" .. file))
+	assert.are.same(
+		vim.fs.abspath("testdata/" .. expected_target),
+		determine_target_file(vim.fs.abspath("testdata/" .. file))
+	)
 end
 
 describe("matching-file.determine_target_file", function()
@@ -23,12 +26,14 @@ describe("matching-file.determine_target_file", function()
 		describe("given test file", function()
 			it("should return matching cs file", function()
 				assert_matching_file("cs/Project/Class.cs", "cs/Project.Test/ClassTest.cs")
+				assert_matching_file("cs/Project/Namespace/Class2.cs", "cs/Project.Test/Namespace/Class2Test.cs")
 			end)
 		end)
 
 		describe("given cs file", function()
 			it("should return matching test file", function()
 				assert_matching_file("cs/Project.Test/ClassTest.cs", "cs/Project/Class.cs")
+				assert_matching_file("cs/Project.Test/Namespace/Class2Test.cs", "cs/Project/Namespace/Class2.cs")
 			end)
 		end)
 	end)
